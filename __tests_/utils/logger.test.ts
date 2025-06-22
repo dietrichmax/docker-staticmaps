7253b24ea4cf88a7afd7d760e5bbcaca654a8ea6
Max Dietrich <mail@mxd.codes>
2025-06-22 17:00:53 +0200

    logger test

--- __tests_/utils/logger.test.ts
+++ __tests_/utils/logger.test.ts
@@ -20,33 +20,6 @@ describe("logger", () => {
     console.error = originalError
   })
 
-  test.each([
-    ["DEBUG", "debug", "debug message", { key: "value" }],
-    ["INFO", "info", "info message", { user: "test" }],
-    ["WARN", "warn", "warn message", {}],
-    ["ERROR", "error", "error message", undefined],
-  ])("logs %s messages with %s method", (level, method, msg, meta) => {
-    // @ts-ignore
-    logger[method](msg, meta)
-    expect(console.log).toHaveBeenCalledTimes(1)
-
-    const expectedPrefix = `\x1b[${
-      {
-        DEBUG: 34,
-        INFO: 32,
-        WARN: 33,
-        ERROR: 31,
-      }[level]
-    }m[2025-06-22T12:34:56.789Z] | [${level}]\x1b[0m | ${msg}`
-
-    const callArg = (console.log as jest.Mock).mock.calls[0][0]
-
-    expect(callArg).toContain(expectedPrefix)
-    if (meta && Object.keys(meta).length > 0) {
-      expect(callArg).toContain(JSON.stringify(meta))
-    }
-  })
-
   test("error method logs stack if error is Error object", () => {
     const error = new Error("test error")
     logger.error(error)
